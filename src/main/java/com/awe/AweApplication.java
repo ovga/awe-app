ab03ff03aec2de9280bfb6ba760202e642d0a148
ovga <ovidiu.ganciu@yahoo.com>
2018-05-02 14:25:48 +0300

    Initial commit for AWE

---
+++ src/main/java/com/awe/AweApplication.java
@@ -0,0 +1,12 @@
+package com.awe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AweApplication.class, args);
+	}
+}
